807d88ce0e95d4fa5600e0ea8dba88860f34ab78
zuzOup <zuzanaoupicka@gmail.com>
2023-11-19 21:35:47 -0500

    .

--- 2022-5/2022-5-update.js
+++ 2022-5/2022-5-update.js
@@ -1,6 +1,7 @@
 const { readFileSync } = require("fs");
-let input = readFileSync("./2022-5-input.txt", "utf-8")
+let input = readFileSync("./2022-5-input-full.txt", "utf-8")
   .split(/\r?\n/)
+  .slice(10)
   .map((x) => x.replace("move ", ""))
   .map((x) => x.replace(" from ", "-stack"))
   .map((x) => x.replace(" to ", "-stack"))
@@ -10,23 +11,35 @@ for (let i = 0; i < input.length; i++) {
   input[i][0] = parseInt(input[i][0]);
 }
 
-//console.log(input);
+function stackMaker() {
+  let inputCrates = readFileSync("./2022-5-input-full.txt", "utf-8").split(
+    /\r?\n/
+  );
 
-function crates() {
-  return {
-    stack1: ["B", "S", "V", "Z", "G", "P", "W"],
-    stack2: ["J", "V", "B", "C", "Z", "F"],
-    stack3: ["V", "L", "M", "H", "N", "Z", "D", "C"],
-    stack4: ["L", "D", "M", "Z", "P", "F", "J", "B"],
-    stack5: ["V", "F", "C", "G", "J", "B", "Q", "H"],
-    stack6: ["G", "F", "Q", "T", "S", "L", "B"],
-    stack7: ["L", "G", "C", "Z", "V"],
-    stack8: ["N", "L", "G"],
-    stack9: ["J", "F", "H", "C"],
-  };
+  const crates = {};
+  const cratesCount = inputCrates[8][inputCrates[8].length - 2];
+
+  for (let i = 1; i <= cratesCount; i++) {
+    crates[`stack${i}`] = [];
+  }
+
+  inputCrates = inputCrates.slice(0, 8);
+
+  for (let i = 0; i < inputCrates.length; i++) {
+    let counter = 0;
+    for (let j = 1; j < inputCrates[i].length; j += 4) {
+      counter++;
+      if (inputCrates[i][j] !== " ") {
+        crates[`stack${counter}`].unshift(inputCrates[i][j]);
+      }
+    }
+  }
+  return crates;
 }
 
-const crates1 = crates();
+const crates1 = stackMaker();
+
+/*---------------------part 1 ----------*/
 
 input.forEach((x) => {
   for (let i = 0; i < x[0]; i++) {
@@ -40,7 +53,7 @@ Object.values(crates1).forEach((x) => (answer1 += x.pop()));
 console.log("Answer 5, 1st half: " + answer1);
 
 /*---------------------part 2 ----------*/
-const crates2 = crates();
+const crates2 = stackMaker();
 
 input.forEach((x) => {
   let stack = [];
