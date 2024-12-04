db57a4883eba3d00bc0213835ed3be2068939968
Zuzana Oupická <zuzanaoupicka@gmail.com>
2024-12-03 19:17:08 -0500

    upload day 3

---
+++ 2024-3/2024_03.js
@@ -0,0 +1,29 @@
+const { readFileSync } = require("fs");
+let input = readFileSync("input2024_03.txt", "utf-8");
+
+/*---------------------------------- PART 1 -----------------------------------------*/
+
+function answer1(string) {
+  return string
+    .split("mul(")
+    .map((x) => x.split(")")[0])
+    .map((x) => x.split(","))
+    .filter((x) => {
+      return x.length === 2 && Number(x[0]) <= 999 && Number(x[1] <= 999); // I probably should've added >0 / >=0 to cover all edge cases, but it worked without it for both parts so....
+    })
+    .map((x) => Number(x[0]) * Number(x[1]))
+    .reduce((acc, cur) => acc + cur, 0);
+}
+
+console.log("Answer 1:" + answer1(input));
+
+/*---------------------------------- PART 2 -----------------------------------------*/
+
+function answer2(string) {
+  return string
+    .split("do()")
+    .map((x) => x.split("don't()")[0])
+    .join();
+}
+
+console.log("Answer2:" + answer1(answer2(input)));
