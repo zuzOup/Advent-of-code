98b4ead4662704e2e605624ef8d29bbfb27fa002
Zuzana Oupická <118570879+zuzOup@users.noreply.github.com>
2025-09-09 11:29:50 -0400

    Update 2024_12.js

--- 2024-12/2024_12.js
+++ 2024-12/2024_12.js
@@ -1,1 +1,56 @@
-const { readFileSync } = require("fs"); let input = readFileSync("input2024_12.txt", "utf-8") 
+const { readFileSync } = require("fs");
+// let input = readFileSync("input2024_12.txt", "utf-8")
+//   .split(/\r?\n/)
+//   .map((x) => x.split(""));
+
+// if (input[input.length - 1].length === 0) {
+//   input.pop();
+// }
+
+// let input = [
+//   ["O", "O", "O", "O", "O"],
+//   ["O", "X", "O", "X", "O"],
+//   ["O", "O", "O", "O", "O"],
+//   ["O", "X", "O", "X", "O"],
+//   ["O", "O", "O", "O", "O"]
+// ]
+
+let input = [
+  ["A", "A", "A", "A"],
+  ["B", "B", "C", "D"],
+  ["B", "B", "C", "C"],
+  ["E", "E", "E", "C"],
+];
+
+function getGardenCoordinates(input) {
+  return input.reduce((garden, row, i) => {
+    row.forEach((field, j) => {
+      (garden[field] ??= []).push([i, j]);
+    });
+    return garden;
+  }, {});
+}
+
+function getPerimeter(plot) {
+
+
+
+  
+  let perimetr = 0;
+
+  for (let i = 0; i < plot.length; i++) {
+      let x =plot[i][0]
+      let y = plot[i][1]
+
+      if (plot)
+
+  }
+    
+    
+    return perimetr;
+}
+
+const garden = getGardenCoordinates(input);
+
+console.log(garden);
+console.log(input);
