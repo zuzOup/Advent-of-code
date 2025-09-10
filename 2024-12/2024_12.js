d572576bc03f13f63eef9855fc26db19e8c1000c
Zuzana Oupická <118570879+zuzOup@users.noreply.github.com>
2025-09-10 10:56:56 -0400

    Update 2024_12.js

--- 2024-12/2024_12.js
+++ 2024-12/2024_12.js
@@ -31,26 +31,49 @@ function getGardenCoordinates(input) {
   }, {});
 }
 
+const arraysEqual = (a, b) => {
+  return a.length === b.length && a.every((xy, i) => xy === b[i]);
+};
+
+const sides = [
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 0],
+];
+
+//             top (x-1, y)
+// left (x, y-1)  [x,y]  right (x, y+1)
+//             bot (x+1,y)
+
 function getPerimeter(plot) {
-
-
-
-  
   let perimetr = 0;
 
   for (let i = 0; i < plot.length; i++) {
-      let x =plot[i][0]
-      let y = plot[i][1]
+    let x = plot[i][0];
+    let y = plot[i][1];
 
-      if (plot)
+    console.log(x, y);
 
+    sides.forEach(side=> {
+      if( plot.some(sqr => arraysEqual(plot[i], plot[i].map((x,i)=> x+side[i])){
+        perimetr++
+      }
+    })
+
+    for (let i = 0; i < 4; i++) {
+      if( plot.some(sides => arraysEqual(sides[i], inner))}
+
+
+    }
   }
-    
-    
-    return perimetr;
+
+  return perimetr;
 }
 
 const garden = getGardenCoordinates(input);
+
+getPerimeter(garden.A);
 
 console.log(garden);
 console.log(input);
